e4a467fda52e7cfa8389ea618fae7c5f4e8a5dd1
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-07-05 10:53:43 -0700

    creating a way to search multiple params at once

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,9 +1,9 @@
-export class WeatherService {
-  getDoctorBy() {
+export class DoctorService {
+  getDoctorBy(listOfParams) {
+    let url = this.createUrl(listOfParams);
+
     return new Promise(function(resolve, reject) {
-      const apiKey = process.env.exports.apiKey;
       let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/conditions?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + apiKey;
 
       request.onload = function() {
         if (this.status === 200) {
@@ -15,5 +15,20 @@ export class WeatherService {
       request.open("GET", url, true);
       request.send();
     });
+  }
+
+  createUrl(listOfParams){
+      const apiKey = process.env.exports.apiKey;
+      let startUrl = 'https://api.betterdoctor.com/2016-03-01/practices?';
+      let endUrl = '100&skip=2&limit=10&user_key=' + apiKey;
+      let middleUrl = '';
+
+      listOfParams.forEach(function(param){
+        middleUrl += param[0] + '=' + param[1] + '&';
+      });
+      console.log(startUrl + middleUrl + endUrl);
+
+      // return startUrl + middleUrl + endUrl;
+      return 'https://api.betterdoctor.com/2016-03-01/practices?name=Cary&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=975cdf755dccdb62421b7e6dbe27b255';
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import $ from 'jquery';
-import DoctorService from './doctor-service';
+import { DoctorService } from './doctor-service';
 
 $(document).ready(function(){
   $(".doctor-search-form").submit(function(e){
     e.preventDefault();
 
     let doctorService = new DoctorService();
-    let promise = doctorService.getDoctorBy();
+    let promise = doctorService.getDoctorBy([['name', 'Cary'], ['location','37.773%2C-122.413%2C100']]);
+    //let promise = doctorService.getDoctorBy([['location','37.773,-122.413']]);
 
     promise.then(function(response){
-      body = JSON.parse(response);
+      let body = JSON.parse(response);
       console.log(body);
     }, function(error){
       console.log('error');
