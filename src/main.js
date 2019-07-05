d246559ea63fbe6fe4efd92a5818d5a54b729d66
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-07-05 09:39:11 -0700

    Called api and got response data

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import $ from 'jquery';
 
 $(document).ready(function(){
-  console.log(process.env.exports.apiKey);
+  const apiKey = process.env.exports.apiKey;
+  const resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key='
+                        + apiKey;
+
+  $.get(resource_url).then(function(response){
+    console.log(response);
+  })
 })
