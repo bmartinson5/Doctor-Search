987ce849869c16bce8c26751a1c64560d24d8401
Benjamin Martinson <benjaminmartinson@iMac-3.local>
2019-07-05 11:49:00 -0700

    added warning about required fields, name or location

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import $ from 'jquery';
+import './styles.css'
 import { DoctorService } from './doctor-service';
 
 $(document).ready(function(){
   $(".doctor-search-form").submit(function(e){
     e.preventDefault();
+    $(".display-param-warning").hide();
 
     let doctorService = new DoctorService();
-    let promise = doctorService.getDoctorBy([['name', 'Cary'], ['location','37.773%2C-122.413%2C100']]);
+    let params = collectParams();
+
+    if(!checkMinParams(params)){
+      $(".display-param-warning").show();
+      return
+    }
+
+    let promise = doctorService.getDoctorBy(params);
     //let promise = doctorService.getDoctorBy([['location','37.773,-122.413']]);
 
     promise.then(function(response){
@@ -16,4 +25,27 @@ $(document).ready(function(){
       console.log('error');
     })
   })
+
 })
+
+function checkMinParams(params){
+  //look for user input of name or location field - need at least one to be filled
+  //required by the better doctor api
+  let found = false;
+  params.forEach(function(param){
+    if(param[0] === 'name' || param[0] === 'location')
+      found = true;
+  })
+  return found;
+}
+
+function collectParams(){
+  let params = [
+                  ['specialty_uid', $("#medical-issue-search").val()],
+                  ['name', $("#name-search").val()],
+                  ['location', $("#location-search").val()]
+                  //['location', '37.773%2C-122.413%2C100']
+               ];
+  //return params that were filled in - not void
+  return params.filter(function(param){ if(param[1]) return param; })
+}
